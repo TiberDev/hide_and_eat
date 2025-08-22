a53e3516950f6d97aa7e36826f18c5e2818020c9
tran.tien <trantien.developer@gmail.com>
2025-08-22 08:15:55 +0700

    Add animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Fusion;
 using Fusion.Addons.SimpleKCC;
 using UnityEngine;
@@ -5,24 +6,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    [SerializeField] private SimpleKCC kcc;
-
-    // [SerializeField] private MeshRenderer[] modelParts;
-    // [SerializeField] private Transform camTarget;
     [SerializeField] private float lookSensitivity = 0.15f;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
-
+    
     [Networked] private NetworkButtons PreviousButtons { get; set; }
 
     private Transform cachedTransform;
+    
+    private PlayerObject playerObj;
+    
+    public SimpleKCC simpleKCC { get; protected set; }
+
+    private void Awake()
+    {
+        playerObj = GetComponent<PlayerObject>();
+    }
 
     public override void Spawned()
     {
+        simpleKCC = GetComponent<SimpleKCC>();
+        
         cachedTransform = transform;
 
-        kcc.Collider.tag = "Player";
-        // kcc.SetGravity(Physics.gravity.y * 2f);
+        simpleKCC.Collider.tag = "Player";
+        simpleKCC.SetGravity(Physics.gravity.y * 2f);
 
         if (HasInputAuthority)
         {
@@ -33,45 +41,22 @@ public class PlayerMovement : NetworkBehaviour
 
     public override void FixedUpdateNetwork()
     {
-        if (GetInput(out PlayerInputData input))
-        {
-            // Vector3 moveDirection = new Vector3(input.Direction.x, 0, input.Direction.y);
-            //
-            // // Get the camera a forward direction
-            // Vector3 camForward = CameraFollow.Instance.Transform.forward;
-            // camForward.y = 0;
-            // camForward.Normalize();
-            //
-            // // Get the camera a right direction
-            // Vector3 camRight = CameraFollow.Instance.Transform.right;
-            // camRight.y = 0;
-            // camRight.Normalize();
-            //
-            // Vector3 moveDir = camForward * moveDirection.z + camRight * moveDirection.x;
-            // moveDir.Normalize();
-            float jump = 0f;
+        if (!GetInput(out PlayerInputData input)) return;
+        float jump = 0f;
 
-            if (input.Buttons.WasPressed(PreviousButtons, InputButton.Jump) && kcc.IsGrounded)
-                jump = jumpForce;
+        if (input.Buttons.WasPressed(PreviousButtons, InputButton.Jump) && simpleKCC.IsGrounded)
+            jump = jumpForce;
             
-            Vector3 moveDirection = new Vector3(input.Direction.x, 0, input.Direction.y);
-            var velocity = moveDirection * speed;
+        Vector3 moveDirection = new Vector3(input.Direction.x, 0, input.Direction.y);
+        var velocity = moveDirection * speed * Runner.DeltaTime;
 
-            PreviousButtons = input.Buttons;
+        PreviousButtons = input.Buttons;
+        simpleKCC.Move(velocity, jump);
 
-            if (Object.HasStateAuthority)
-            {
-                kcc.Move(velocity, jump);
-                Debug.Log(
-                    $"Instance ID: {GetInstanceID()} velocity: {velocity}");
-            }
-
-            // // Rotate the player to face the movement direction
-            // if (moveDir.x == 0 && moveDir.z == 0) return;
-            // Vector3 lookDir = new Vector3(-moveDir.x, 0, -moveDir.z);
-            // Debug.Log($"Instance ID: {GetInstanceID()}  Look Direction: {lookDir}");
-            // Quaternion targetRot = Quaternion.LookRotation(lookDir);
-            // kcc.SetLookRotation(Quaternion.Slerp(cachedTransform.rotation, targetRot, 0.2f));
-        }
+        // Rotate the player to face the movement direction
+        if (velocity.x == 0 && velocity.z == 0) return;
+        Vector3 lookDir = new Vector3(-velocity.x, 0, -velocity.z);
+        Quaternion targetRot = Quaternion.LookRotation(lookDir);
+        simpleKCC.SetLookRotation(Quaternion.Slerp(cachedTransform.rotation, targetRot, 0.2f));
     }
 }
--- Assets/Scripts/PlayerObject.cs
+++ Assets/Scripts/PlayerObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Fusion;
@@ -5,27 +6,36 @@ using UnityEngine;
 
 public class PlayerObject : NetworkBehaviour
 {
+    private static readonly int MOVE_SPEED_ANIM_PARAMATER = Animator.StringToHash("moveSpeed");
+
     [Networked, OnChangedRender(nameof(OnNicknameChanged))]
     public NetworkString<_16> Nickname { get; set; }
-    
+
+    [SerializeField] private Animator anim;
+
+    private PlayerMovement playerMovement;
+
+    float moveSpeed = 0;
+
     public override void Spawned()
     {
-        base.Spawned();
-
+        playerMovement = GetComponent<PlayerMovement>();
         if (Object.HasInputAuthority)
         {
             // Set the player name from PlayerPrefs or a default value
+            gameObject.name = Nickname.Value;
             Rpc_SetPlayerName(StartupNetworkController.Instance.NickName);
         }
         else
         {
             if (!string.IsNullOrEmpty(Nickname.Value))
             {
+                gameObject.name = Nickname.Value;
                 GameManager.Instance.CreateWorldCanvasNickname(Nickname.Value, transform, false);
             }
         }
     }
-    
+
     [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
     void Rpc_SetPlayerName(string nickName)
     {
@@ -34,6 +44,19 @@ public class PlayerObject : NetworkBehaviour
 
     private void OnNicknameChanged()
     {
+        gameObject.name = Nickname.Value;
         GameManager.Instance.CreateWorldCanvasNickname(Nickname.Value, transform, Object.HasInputAuthority);
+    }
+
+    public override void Render()
+    {
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        moveSpeed = Mathf.Min(1f,
+            Mathf.MoveTowards(moveSpeed, playerMovement.simpleKCC.RealSpeed, Time.deltaTime * 3f));
+        anim.SetFloat(MOVE_SPEED_ANIM_PARAMATER, moveSpeed);
     }
 }
